50c1bdbd842f58671e9fbf22d470949fef88bd9e
Shohin <shohinalimov2008@gmail.com>
2025-09-25 21:06:03 +0500

    Commit

---
+++ Task3/script.js
@@ -0,0 +1,40 @@
+const { createServer } = require("http");
+const { parse } = require("path");
+const url = require("url");
+const emailPath = "/shohinalimov2008_gmail_com";
+
+const a = 12;
+const b = 18;
+
+function NOD(a, b) {
+  while (b !== 0) {
+    [a, b] = [b, a % b];
+  }
+  return a;
+}
+
+function NOK(a, b) {
+  return (a * b) / NOD(a, b);
+}
+
+const server = createServer((req, res) => {
+  const parsed = url.parse(req.url, true);
+
+  if (parsed.pathname === emailPath) {
+    let x = parseInt(parsed.query.x);
+    let y = parseInt(parsed.query.y);
+
+    if (!Number.isInteger(x) || !Number.isInteger(y) || x <= 0 || y <= 0) {
+      res.end("NaN");
+    } else {
+      res.end(NOK(x, y).toString());
+    }
+  } else {
+    res.statusCode = 404;
+    res.end("NaN");
+  }
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Server running...");
+});
