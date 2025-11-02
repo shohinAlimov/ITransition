1f32bcd44133db80aeecf6aba59f48d202324842
Shohin <shohinalimov2008@gmail.com>
2025-11-02 14:17:37 +0500

    chore: remove old _redirects file and add new version in public directory

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,9 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  publicDir: "public",
-  build: {
-    outDir: "dist",
-    copyPublicDir: true,
-  },
 });
