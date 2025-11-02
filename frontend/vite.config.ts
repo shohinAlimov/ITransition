178e9053b57b3db8e1b9b4301d94abeb6072ba1c
Shohin <shohinalimov2008@gmail.com>
2025-11-02 14:01:37 +0500

    fix: correct configuration settings in vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  publicDir: "public",
+  build: {
+    outDir: "dist",
+    copyPublicDir: true,
+  },
 });
